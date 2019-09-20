10e90af9951dfd8d36aa5bd5436d77fdaa17ff8e
52999103 <kahwaji.anthony@gmail.com>
2019-09-20 12:13:19 +0200

    update the file hardly

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 const button = document.getElementById('enter');
 const input = document.getElementById('userinput');
 const ul = document.getElementsByTagName('ul');
-let returning = document.createTextNode(`[$returning]`);
+let returning = document.createTextNode(input.value);
+
 
 function inputLength () {
     return input.value.length;
 }
+
+
+    function CreatedElementPlayer () {
+        const li = document.createElement('li');
+        li.appendChild(returning);
+        ul.appendChild(li);
+	    input.value = "";
+    }
+
+
+    function DetectedMouse () {
+        if ( inputLenght > 0 ){
+            CreatedElementPlayer();
+        }
+    }
+
+
+    function DetectedKeypress (ENTER) {
+        if ( inputLength > 0 && ENTER.keyCode || ENTER.which === 13 ) {
+            CreatedElementPlayer();
+        }
+    }
+
+
+    button.addEventListener("click", DetectedMouse);
+    input.addEventListener("keypress", DetectedKeypress);
